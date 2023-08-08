e3ee614fb46c02739344b1bf1bc5196fae1f36f6
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2023-08-08 13:21:12 -0700

    getting rid of other taskslists + width variable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,39 +7,20 @@
 	todo.add(new Task("skdljf", "sdlkjfsdkfs"));
 	done.add(new Task("sjdsj", "kdsjf"));
 
-	// Should move this logic elsewhere
-	let screenWidth = 0;
-	let width;
-	$: {
-		width = Math.max(0.2 * screenWidth, 200);
-		console.log(screenWidth);
-	}
-	$: console.log(width);
-	let num = 0;
-	let bruh = () => {
-		todo.add(new Task(num, "kdsjl"));
-		console.log("bruh");
-		num += 1;
-	};
 </script>
 
-<svelte:window bind:innerWidth={screenWidth} />
 
 <h1>Todo List</h1>
 
 <div class="container">
-	<TaskListEle taskList={todo} {width}/>
-	<TaskListEle taskList={done} {width}/>
-	<button on:click={bruh} style:width={width}>+ Add New List</button>
+	<TaskListEle taskList={todo}/>
 </div>
 
 <style>
 	.container {
 		display: flex;
+		justify-content: center;
 		width: 100%;
 		background-color: yellow;
-	}
-	button {
-		background-color: red;
 	}
 </style>
