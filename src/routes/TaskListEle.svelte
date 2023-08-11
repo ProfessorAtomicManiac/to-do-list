87bb6b149f5829d2e753c3dcc3fea52f43b0415d
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2023-08-11 14:08:02 -0700

    aligned trash can icon

--- src/routes/TaskListEle.svelte
+++ src/routes/TaskListEle.svelte
@@ -46,12 +46,13 @@
     <ul>
         {#each $tasks as task, i}
             <li>
-                <span>{task.getTitle()}</span>
+                <span class="task-name">{task.getTitle()}</span>
+                <span class="hidden"></span>
                 <button on:click={() => del(i)} aria-label="Remove">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
-                      </svg>
+                    </svg>
                     
                 </button>
             </li>
@@ -101,14 +102,20 @@
         --task-border-width: 1px;
         --task-border-color: black;
         --task-margin: 5px;
-        --task-padding: 5px;
+        --task-padding: 3px;
+        --task-name-margin: calc(5px - var(--task-padding));
         --task-background-color: #fff;
+        --task-height: 2em;
 
         display: flex;
         border: var(--task-border-width) solid var(--task-border-color);
         margin: var(--task-margin);
         padding: var(--task-padding);
-        background-color: var(--task-background-color);    
+        background-color: var(--task-background-color); 
+        height: var(--task-height);   
+    }
+    .task-name {
+        margin: var(--task-name-margin);
     }
     li:last-of-type {
         padding: 0;
@@ -117,34 +124,38 @@
         width: 100%;
     }
     input {
-        padding: var(--task-padding);
+        padding: calc(var(--task-padding) + var(--task-name-margin));
         flex: 1;
         width: 100%;
         border: none;
     }
-    span {
+    .hidden {
         flex: 1;
     }
     button {
         --btn-border-width: 0;
-        --btn-border-color: #90979d;
+        --btn-border-color: #dadcde;
         --btn-bg-color: #ffffff;
-        --btn-hover-bg-color: #6c757d;
+        --btn-hover-bg-color: #dadcde;
+        /* would realy like to make --btn-side-length: 100% but it sets it to 16px, no idea why */
+        --btn-side-length: 100%;
+        --btn-trash-can-color: #6c757d;
         background-color: var(--btn-bg-color);
+        color: var(--btn-trash-can-color);
         text-align: center;
+        align-self: center;
         padding: 0;
         margin: 0;
         aspect-ratio: 1 / 1;
-        height: 20px;
+        height: calc(var(--task-height) - var(--task-padding));
         border: var(--btn-border-width) solid var(--btn-border-color);
 	}
     svg {
         background-color: transparent;
-        vertical-align: sub;
+        vertical-align: middle;
         margin: 0;
     }
     button:hover {
         background-color: var(--btn-hover-bg-color);
-        color: white;
     }
 </style>
