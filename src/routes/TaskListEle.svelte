76a2eb46156b8416b88308cfed54234c2b2d762a
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2023-08-11 13:21:17 -0700

    Added trash icon

--- src/routes/TaskListEle.svelte
+++ src/routes/TaskListEle.svelte
@@ -47,7 +47,13 @@
         {#each $tasks as task, i}
             <li>
                 <span>{task.getTitle()}</span>
-                <button on:click={() => del(i)} aria-label="Remove">delete</button>
+                <button on:click={() => del(i)} aria-label="Remove">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
+                      </svg>
+                    
+                </button>
             </li>
         {/each}
         <li>
@@ -119,8 +125,25 @@
         flex: 1;
     }
     button {
-        background-color: white;
+        --btn-border-width: 0;
+        --btn-border-color: #90979d;
+        --btn-bg-color: #ffffff;
+        --btn-hover-bg-color: #6c757d;
+        background-color: var(--btn-bg-color);
+        text-align: center;
+        padding: 0;
+        margin: 0;
         aspect-ratio: 1 / 1;
         height: 20px;
-	}   
+        border: var(--btn-border-width) solid var(--btn-border-color);
+	}
+    svg {
+        background-color: transparent;
+        vertical-align: sub;
+        margin: 0;
+    }
+    button:hover {
+        background-color: var(--btn-hover-bg-color);
+        color: white;
+    }
 </style>
